4f3f5ae19b017f83f5d97bc39c3e9c6af9041e95
codingchrissi <137509246+codingchrissi@users.noreply.github.com>
2023-06-23 14:26:48 +0200

    Update YangZhang.py
    According to the original paper, open_vol and close_vol are the demeaned sum over the respective window. This can be reached by demeaning the sequences log_oc_sq and log_cc_sq.
    Reference: https://www.jstor.org/stable/pdf/10.1086/209650.pdf

--- volatility/models/YangZhang.py
+++ volatility/models/YangZhang.py
@@ -10,10 +10,10 @@ def get_estimator(price_data, window=30, trading_periods=252, clean=True):
     log_co = (price_data['Close'] / price_data['Open']).apply(np.log)
     
     log_oc = (price_data['Open'] / price_data['Close'].shift(1)).apply(np.log)
-    log_oc_sq = log_oc**2
+    log_oc_sq = (log_oc - np.nanmean(log_oc))**2
     
     log_cc = (price_data['Close'] / price_data['Close'].shift(1)).apply(np.log)
-    log_cc_sq = log_cc**2
+    log_cc_sq = (log_cc - np.nanmean(log_cc))**2
     
     rs = log_ho * (log_ho - log_co) + log_lo * (log_lo - log_co)
     
