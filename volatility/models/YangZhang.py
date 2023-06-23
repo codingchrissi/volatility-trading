b3755ff945ddfb2d3bbafeeae7b58b65a4385c80
codingchrissi <137509246+codingchrissi@users.noreply.github.com>
2023-06-23 14:47:31 +0200

    Update YangZhang.py
    Combined with another suggestion some minutes ago: 
    The series log_cc does not occur in the original paper. Instead, they use log_co. Further, the squared series' are to be demeaned.
    Reference: https://www.jstor.org/stable/pdf/10.1086/209650.pdf

--- volatility/models/YangZhang.py
+++ volatility/models/YangZhang.py
@@ -7,17 +7,21 @@ def get_estimator(price_data, window=30, trading_periods=252, clean=True):
 
     log_ho = (price_data['High'] / price_data['Open']).apply(np.log)
     log_lo = (price_data['Low'] / price_data['Open']).apply(np.log)
+    
     log_co = (price_data['Close'] / price_data['Open']).apply(np.log)
+    log_co_sq = (log_co - np.mean(log_co))**2
     
     log_oc = (price_data['Open'] / price_data['Close'].shift(1)).apply(np.log)
-    log_oc_sq = log_oc**2
-    
+    log_oc_sq = (log_oc - np.mean(log_oc))**2
+
+    '''  not necessary
     log_cc = (price_data['Close'] / price_data['Close'].shift(1)).apply(np.log)
-    log_cc_sq = log_cc**2
+    log_cc_sq = log_cc**2 
+    '''
     
     rs = log_ho * (log_ho - log_co) + log_lo * (log_lo - log_co)
     
-    close_vol = log_cc_sq.rolling(
+    close_vol = log_co_sq.rolling(
         window=window,
         center=False
     ).sum() * (1.0 / (window - 1.0))
